14fc5635f2edb94fbefae13f2158060cf13a6566
Mathilde <mkerhom@gmail.com>
2017-04-15 17:15:03 +0200

    test ContactList

---
+++ Swaguin/Swaguin/Swaguin/Models/ContactFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Swaguin.Models
+{
+	public class ContactFactory
+	{
+		public static List<Contact> Contacts { get; private set; }
+
+		static ContactFactory()
+		{
+			Contacts = new ObservableCollection<Contact> {
+				new Contact{
+					FirstName = "Stéphanie",
+					LastName = "Blondeau",
+					Gender = false,
+					Favorite = false,
+					Email = "stephanie.blondeau@gmail.com",
+					Birthdate = new DateTime(),
+					PhoneNumber = 6616984830,
+					ImagePath = ""
+				}
+			};
+		}
+	}
+}
